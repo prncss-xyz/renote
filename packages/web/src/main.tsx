3ed330e8ea50af40f9309c5d67c164a2b1471942
Juliette Lamarche <juliette.lamarche.xyz@gmail.com>
2024-07-18 16:39:34 -0400

    fix: basepath

--- packages/web/src/main.tsx
+++ packages/web/src/main.tsx
@@ -22,6 +22,7 @@ const context: MyRooterContext = {
 }
 
 const router = createRouter({
+	basepath: '/renote/',
 	routeTree,
 	context,
 	defaultPreloadStaleTime: 0,
@@ -36,7 +37,7 @@ declare module '@tanstack/react-router' {
 
 const rootElement = document.getElementById('root')!
 if (!rootElement.innerHTML) {
-	const root = ReactDOM.createRoot(rootElement)
+	const root = ReactDOM.createRoot(rootElement, {})
 	root.render(
 		<StrictMode>
 			<QueryClientProvider client={queryClient}>
