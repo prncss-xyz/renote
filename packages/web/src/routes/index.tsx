fe0fa6c99329224c9251929c5cdd236edaa32083
Juliette Lamarche <juliette.lamarche.xyz@gmail.com>
2024-07-20 13:28:11 -0400

    fix(404): redirect

--- packages/web/src/routes/index.tsx
+++ packages/web/src/routes/index.tsx
@@ -1,32 +1,15 @@
-import { createFileRoute, useSearch } from '@tanstack/react-router'
-import { useEffect } from 'react'
+import { createFileRoute, Navigate, useSearch } from '@tanstack/react-router'
 import { z } from 'zod'
 
 export const Route = createFileRoute('/')({
 	component: Component,
-	validateSearch: z.object({ path: z.string().catch('') }),
+	validateSearch: z.object({
+		to: z.string().catch('/notes'),
+	}),
 })
 
-// this is to support github pages
-// https://github.com/rafgraph/spa-github-pages/blob/gh-pages/index.html#L21-L42
-function decodePath(path: string) {
-	return path
-		.slice(1)
-		.split('&')
-		.map(function (s) {
-			return s.replace(/~and~/g, '&')
-		})
-		.join('?')
-}
-
 function Component() {
-	const { path } = useSearch({ from: Route.fullPath })
-	const to = `${import.meta.env.BASE_URL}/${path ? decodePath(path) : '/notes'}`
-	// __AUTO_GENERATED_PRINT_VAR_START__
+	const { to } = useSearch({ from: Route.fullPath })
 	console.log('Component to: %s', to) // __AUTO_GENERATED_PRINT_VAR_END__
-	useEffect(() => {
-		console.log('effect')
-		window.location.replace(to)
-	}, [to])
-	return null
+	return <Navigate to={to} />
 }
