76eea91f6750c7035c4836e2633d6953e724d287
Juliette Lamarche <juliette.lamarche.xyz@gmail.com>
2024-07-20 10:18:21 -0400

    fix: revert basepath handling

--- packages/web/vite.config.ts
+++ packages/web/vite.config.ts
@@ -3,12 +3,19 @@ import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 
-// bump
-console.log('>>> BASE_PATH: %s', process.env.BASE_PATH)
+// this is specific to the github pages
+
+function getBasePath() {
+	const repo = process.env.GITHUB_REPOSITORY
+	if (!repo) return undefined
+	const [, name] = repo.split('/')
+	if (!name) return undefined
+	return `/${name}`
+}
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	base: process.env.BASE_PATH,
+	base: getBasePath(),
 	plugins: [TanStackRouterVite(), react()],
 	resolve: {
 		alias: {
