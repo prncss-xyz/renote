cbcc26e658056417d615f666980f63da161bb25a
Juliette Lamarche <juliette.lamarche.xyz@gmail.com>
2024-07-20 12:51:02 -0400

    fix(404): redirect

--- packages/web/src/routes/index.tsx
+++ packages/web/src/routes/index.tsx
@@ -21,9 +21,12 @@ function decodePath(path: string) {
 
 function Component() {
 	const { path } = useSearch({ from: Route.fullPath })
-	const to = path ? decodePath(path) : '/notes'
+	const to = `${import.meta.env.BASE_URL}/${path ? decodePath(path) : '/notes'}`
+	// __AUTO_GENERATED_PRINT_VAR_START__
+	console.log('Component to: %s', to) // __AUTO_GENERATED_PRINT_VAR_END__
 	useEffect(() => {
-		window.location.replace(`${import.meta.env.BASE_URL}/${to}`)
+		console.log('effect')
+		window.location.replace(to)
 	}, [to])
 	return null
 }
